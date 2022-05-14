e7d75fc330ccc1a6578c1ded68be0e9bdb0b161a
HELLORPG <hellorpg2017@gmail.com>
2022-05-14 20:45:31 +0800

    Add BookingInfo init from info dict.

--- data/bookingInfo.py
+++ data/bookingInfo.py
@@ -26,9 +26,40 @@ class BookingInfo:
         self.__has_price = False
         self.price = None
 
+    def __init__(self, info: dict):
+        """
+        使用info字典来初始化实例
+        :param info: booking info in dict.
+        """
+        self.description = info["description"]
+        self.neighbor = info["info"]
+        self.latitude = info["latitude"]
+        self.longitude = info["longitude"]
+        self.type = info["type"]
+        self.accommodates = info["accommodates"]
+        self.bathrooms = info["bathrooms"]
+        self.bedrooms = info["bedrooms"]
+        self.amenities = info["amenities"]
+        self.reviews = info["reviews"]
+        self.review_rating = info["review_rating"]
+        self.review_A = info["review_A"]
+        self.review_B = info["review_B"]
+        self.review_C = info["review_C"]
+        self.review_D = info["review_D"]
+        self.instant_bookable = info["instant_bookable"]
+
+        if "price" in info:
+            self.__has_price = True
+            self.price = info["price"]
+
     def _price(self):
         if self.__has_price:
             return self.price
         else:
             print("This info has no self.price.")
             exit(-1)
+
+    def _has_price(self):
+        return self.__has_price
+
+
