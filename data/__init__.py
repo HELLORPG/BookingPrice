9aeaeddd275e8406e6c1ae3fbc378d4755c7c3cc
HELLORPG <hellorpg2017@gmail.com>
2022-05-14 17:17:00 +0800

    Add 'data' package

---
+++ data/__init__.py
@@ -0,0 +1,5 @@
+# data package 用来对数据进行处理
+
+__all__ = ["bookingInfo"]
+
+from .bookingInfo import BookingInfo    # 使用类似的代码，可以减少引用BookingInfo Class时所需的字段层次，from data import BookingInfo 就可以。
---
+++ data/bookingInfo.py
@@ -0,0 +1,5 @@
+# 用来表示房屋预定信息
+
+class BookingInfo:
+    def __init__(self):
+        return
