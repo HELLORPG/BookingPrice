c6b8f297e0c56da16099d70763341e5e1175d835
HELLORPG <hellorpg2017@gmail.com>
2022-05-17 20:08:38 +0800

    Rename opFile.py to dataFile.py.

--- data/__init__.py
+++ data/__init__.py
@@ -1,6 +1,6 @@
 # data package 用来对数据进行处理
 
 from .bookingInfo import BookingInfo    # 使用类似的代码，可以减少引用BookingInfo Class时所需的字段层次，from data import BookingInfo 就可以。
-from .opFile import DataFile
+from .dataFile import DataFile
 
-__all__ = ["bookingInfo", "opFile", "location", "utils", "BookingInfo", "DataFile"]
+__all__ = ["bookingInfo", "dataFile.py", "location", "utils", "BookingInfo", "DataFile"]
