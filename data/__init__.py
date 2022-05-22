46ae611ff9b1617fe345e9d8db264078be7367b0
HELLORPG <hellorpg2017@gmail.com>
2022-05-22 10:54:20 +0800

    Fix import problem.

--- data/__init__.py
+++ data/__init__.py
@@ -1,7 +1,9 @@
 # data package 用来对数据进行处理
 
-from .bookingInfo import BookingInfo    # 使用类似的代码，可以减少引用BookingInfo Class时所需的字段层次，from data import BookingInfo 就可以。
 from .infoFile import InfoFile
+from .bookingInfo import BookingInfo    # 使用类似的代码，可以减少引用BookingInfo Class时所需的字段层次，from data import BookingInfo 就可以。
+from .importantAmenity import ImportantAmenity
 from .bookingToken import BookingToken
 
-__all__ = ["bookingInfo", "infoFile", "location", "utils", "BookingInfo", "InfoFile", "bookingToken", "BookingToken"]
+__all__ = ["bookingInfo", "infoFile", "location", "utils", "BookingInfo", "InfoFile", "bookingToken", "BookingToken",
+           "ImportantAmenity"]
