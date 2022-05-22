a6a48e1dec1e126066308fb209e3722ea2ad9696
HELLORPG <hellorpg2017@gmail.com>
2022-05-22 21:24:08 +0800

    Add some args.

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ import argparse
 
 from data.location import location_scatter_from_booking_info_file
 from data.dataset import split_train_val
+from engine import train
 
 
 def run_args_parser() -> argparse.ArgumentParser:
@@ -21,13 +22,38 @@ def run_args_parser() -> argparse.ArgumentParser:
     args_parser.add_argument("--output-path", type=str)
 
     # 对于split_train_val独有的参数
-    args_parser.add_argument("--train-path", type=str)      # 和train过程中的train path共用
-    args_parser.add_argument("--val-path", type=str)        # 和train过程中的val path共用
+    args_parser.add_argument("--train-path", type=str, default="./dataset/split/train.csv")     # 和train过程中的train path共用
+    args_parser.add_argument("--val-path", type=str, default="./dataset/split/val.csv")         # 和train过程中的val path共用
     args_parser.add_argument("--train-ratio", type=float)
     args_parser.add_argument("--train-size", type=int)
     args_parser.add_argument("--val-size", type=int)
 
-    # 对于训练使用的参数
+    # 对于训练和测试可能使用的参数 ===================================================================================
+    # 设备
+    args_parser.add_argument("--device", type=str, default="cpu")
+
+    # 输出文件
+    args_parser.add_argument("--data-meta-dir", type=str, default="./outputs/meta")
+
+    # 是否增加验证输出
+    args_parser.add_argument("--with-val", type=str, default="True", help="Train with eval val.")
+    # args_parser.add_argument("--with-test", type=str, default="True", help="Test at final.")
+
+    # 超参数
+    args_parser.add_argument("--batch-size", type=int, default=10)
+    args_parser.add_argument("--epoch", type=int, default=100)
+
+    # 优化器
+    args_parser.add_argument("--optimizer", type=str, default="Adam")
+    args_parser.add_argument("--lr", type=float, default=1e-4)
+    args_parser.add_argument("--weight-decay", type=float, default=1e-5)
+
+    # 损失函数
+    args_parser.add_argument("--loss-function", type=str, default="SmoothL1")
+
+    # 模型结构
+    args_parser.add_argument("--activation", type=str, default="relu")
+    args_parser.add_argument("--position-mlp-layers", default=[80, 20], type=int, nargs="+")
 
     return args_parser
 
@@ -70,6 +96,8 @@ def main(args):
         else:
             print("Do Not Know how to split dataset.")
             exit(-1)
+    elif args.part == "train":
+        train(args=args)
     else:
         print("run.py do not support part: %s." % args.part)
         exit(-1)
